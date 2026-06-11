b50495c890c8ba42a944841632fa26a893ae1d98
Vybyranyi <mariankvit@gmail.com>
2026-06-11 19:33:09 +0300

    feat: add alias for shared components in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@redmonkey/shared': path.resolve(__dirname, '../shared/src'),
     },
   },
 })
